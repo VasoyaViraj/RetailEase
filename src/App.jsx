99b35c550bfc73faaf6219572872fbbcac965e07
VasoyaViraj <virajvasoya1@gmail.com>
2025-04-27 11:28:01 +0530

    router_N_routes_setuped

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { Button } from "@/components/ui/button"
+import BillingPage from './Pages/BillingPage';
+import AddProduct from './Pages/Admin/AddProduct';
+import ProductList from './Pages/Admin/ProductList';
+import ScanProduct from './Pages/Admin/ScanProduct';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return(
     <>
-      <div className="flex justify-center items-center">RetailEase</div>
-      <Button>Click me</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BillingPage/>} />
+          <Route path="/admin/addproduct" element={<AddProduct/> }/>
+          <Route path="/admin/productlist" element={<ProductList/> }/>
+          <Route path="/admin/scanproduct" element={<ScanProduct/> }/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
